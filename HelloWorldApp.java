b41d091a24055016077fde1bd3e501d75454cc1a
Avi22022 <47929384+Avi22022@users.noreply.github.com>
2019-03-03 13:44:59 -0700

    updated

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,7 +3,7 @@
  * simply prints "Hello World!" to standard output. this is a comment section for testing
  */
 class HelloWorldApp {
-    public static void main(String[] args) {
+    public static void main(String[ args) {
         System.out.println("Hello World!"); // Display the string.
     }
 }
