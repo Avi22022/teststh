18c75c5a95bfc12437f308c568d8eaf8cb8f5690
Avi22022 <47929384+Avi22022@users.noreply.github.com>
2019-03-03 13:44:06 -0700

    updated helloworld for testing

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,6 +1,6 @@
 /**
  * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output. this is a comment section
+ * simply prints "Hello World!" to standard output. this is a comment section for testing
  */
 class HelloWorldApp {
     public static void main(String[] args) {
